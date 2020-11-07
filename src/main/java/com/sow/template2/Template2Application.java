6c44d5eeb903cb4543f197569109fa062dd729d4
vjteky <vjteky@gmail.com>
2020-11-07 12:10:14 -0500

    initial checkin

---
+++ src/main/java/com/sow/template2/Template2Application.java
@@ -0,0 +1,13 @@
+package com.sow.template2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Template2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Template2Application.class, args);
+	}
+
+}
